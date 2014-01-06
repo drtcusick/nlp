6c14dbef0d3747ba03707d9e2bea882f8036198d
Travis Travis <travisemail@emailserv.com>
2014-01-05 20:52:02 -0600

    List of required libraries added.  The libraries are not in the github repository due to size

---
+++ dots/lib/README.txt
@@ -0,0 +1,17 @@
+
+The following jar files are required for the dots project
+
+ejml-0.23.jar
+junit.jar
+org.hamcrest.core_1.1.0.v20090501071000.jar
+README.txt
+stanford-classifier.jar
+stanford-classifier-3.3.0.jar
+stanford-classifier-3.3.0-javadoc.jar
+stanford-classifier-3.3.0-sources.jar
+stanford-corenlp-3.3.0.jar
+stanford-corenlp-3.3.0-models.jar
+stanford-parser.jar
+stanford-parser-3.3.0-javadoc.jar
+stanford-parser-3.3.0-models.jar
+stanford-parser-3.3.0-sources.jar
--- dots/src/test/java/com/asynchrony/nlp/dots/DotMainTest.java
+++ dots/src/test/java/com/asynchrony/nlp/dots/DotMainTest.java
@@ -2,7 +2,23 @@ package com.asynchrony.nlp.dots;
 
 import org.junit.Before;
 import org.junit.Test;
+/*
+ REQUIRED LIBRARIES:
+ejml-0.23.jar
+junit.jar
+org.hamcrest.core_1.1.0.v20090501071000.jar
+stanford-classifier.jar
+stanford-classifier-3.3.0.jar
+stanford-classifier-3.3.0-javadoc.jar
+stanford-classifier-3.3.0-sources.jar
+stanford-corenlp-3.3.0.jar
+stanford-corenlp-3.3.0-models.jar
+stanford-parser.jar
+stanford-parser-3.3.0-javadoc.jar
+stanford-parser-3.3.0-models.jar
+stanford-parser-3.3.0-sources.jar
 
+*/
 public class DotMainTest {
 
 	private static final String TEST_SENTENCE = "Bob was able to see that the machine was broken";
